6606df631a88e86eb022b0b4ccc8c36a9f457db7
Laseandra Kelly <Seanie@Laseandras-MacBook-Pro.local>
2017-09-13 13:21:08 -0400

    second

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+    public static void main( String[] args)
+    {
+        System.out.println("+-----------------------------------+");
+        System.out.println("|    Java Bank ATM Receipt          |");
+        System.out.println("|    Wednesday, September 13, 2017  |");
+        System.out.println("|    ATM Location 547               |");
+        System.out.println("|                                   |");
+        System.out.println("|                                   |");
+        System.out.println("|   Account Number:    1234         |");
+        System.out.println("|                                   |");
+        System.out.println("+-----------------------------------+");
+    }
+}
